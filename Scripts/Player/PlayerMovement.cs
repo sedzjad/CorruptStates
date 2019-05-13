5634ae1e1817b58855bbc3a4bf97d5767ff0e824
sedzjad <31769691+sedzjad@users.noreply.github.com>
2019-05-13 10:22:22 +0200

    Add files via upload

---
+++ Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5;
+
+    void Update()
+    {
+        _PlayerMovement();
+    }
+
+    private void _PlayerMovement()
+    {
+        float HorizontalMovement = _moveSpeed * Input.GetAxisRaw("Horizontal");
+        float VerticalMovement = _moveSpeed * Input.GetAxisRaw("Vertical");
+        transform.Translate(HorizontalMovement, 0f, VerticalMovement);
+    }
+}
